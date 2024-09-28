1899ecb356299617450e1dbdb87dcdefb14bec79
Abhishadas <abhishadas9004@gmail.com>
2024-09-28 14:29:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar/NavBar";
 import './App.css'
 import Banner from "./components/Banner/Banner";
 import RowPost from "./components/RowPost/RowPost";
-import {action, originals } from './urls'
+import {action, originals, comedy } from './urls'
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Banner/>
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall />
+      {/* <RowPost url={comedy} title='Comedy' isSmall /> */}
     </div>
   );
 }
